b2bba50fd140f1e6336166e19bedeb751a7a9a27
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-02-15 19:48:38 +0100

    19.3 added Delete method

--- src/main/java/com/crud/tasks/controller/GlobalHttpErrorHandler.java
+++ src/main/java/com/crud/tasks/controller/GlobalHttpErrorHandler.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.controller;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,5 +13,10 @@ public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(TaskNotFoundException.class)
     public ResponseEntity<Object> handleTaskNotFoundException(TaskNotFoundException exception) {
         return new ResponseEntity<>("Task with given id does not exist", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException exception) {
+        return new ResponseEntity<>("Trying to delete task, that does not exist", HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -4,6 +4,7 @@ import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,5 +26,9 @@ public class DbService {
 
     public Task saveTask(final Task task) {
         return repository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        repository.deleteById(id);
     }
 }
