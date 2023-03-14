1c5dd70e32a485836f72a68a257c1f7b4e8b3c84
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-03-14 19:21:14 +0100

    Adding autodeploy on replit

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -2,7 +2,6 @@ package com.crud.tasks.trello.client;
 
 import com.crud.tasks.domain.*;
 import com.crud.tasks.trello.config.TrelloConfig;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
